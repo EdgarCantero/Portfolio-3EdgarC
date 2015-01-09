aa9a24920523fc15b5ee7263c1a8ffc5988330c4
Edgar Cantero <cantero098@gmail.com>
2015-01-09 09:16:46 -0800

    battery  at 1 percent dont think i can make it hEELLPP

--- public_html/Scripts.js
+++ public_html/Scripts.js
@@ -6,4 +6,15 @@
 
 $("document") .ready (function(){
     $(".class").css("color", "yellow").css("font-style","italic").css("background-color", "gray") ;
+    $('p:last').css("color", "blue").css("font-style","");
+    
+    $('p:gt(4)').css('border-style', 'solid');
+    $("h1").bind('mouseover',mouseOverMe).bind('mouseout',mouseOutMe);
 });
+function mouseOverMe(){
+    $("h1");
+}
+
+function mouseOutMe(){
+    $("h1").html("TOLD YOU DON'T TOUCH")
+}
